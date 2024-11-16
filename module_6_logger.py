88ef4db0b36daa8c8eb4df0bb67a72e2f4466624
Vladimir N <evaeva6202@gmail.com>
2024-11-16 21:14:39 +0300

    module_6_logger прислал Мороз я опоздал на 30 мин
    module_6_logger прислал Мороз после моего опоздания на 30 минут

---
+++ module_6_logger.py
@@ -0,0 +1,67 @@
+# Прислал Мороз после лекции где я опоздал на 30 минут
+'''
+Логгер — это инструмент или компонент, который используется для записи событий, происходящих в программе.
+Логирование помогает разработчикам и системным администраторам отслеживать выполнение программы, находить ошибки,
+диагностировать проблемы, а также понимать, как программа работает в различных условиях.
+'''
+
+from datetime import datetime
+
+class Logger:
+    def __init__(self):
+        self.logs = []
+
+    def log(self, message):
+        time = datetime.now().strftime("%H:%M:%S")
+        self.logs.append(f"LOG from {time} " + message)
+
+
+
+class AccessControl:
+    def __init__(self, user):
+        self.user = user
+        self.permissions = {"read": True, "write": False}
+
+    def has_permission(self, action):
+        return self.permissions.get(action, False)
+
+    def change_permission(self, action, value=bool()):
+
+        if action in self.permissions:
+            self.permissions[action] = value
+            print(f"Модификатор доступа '{action}' изменен на {value} для пользователя {self.user}.")
+        else:
+            print(f"Неверный типа модификатора доступа: {action}. Доступные модификаторы доступа: {list(self.permissions.keys())}.")
+
+
+class SecureResource(Logger, AccessControl):
+    def __init__(self, user, resource_name):
+        Logger.__init__(self)
+        AccessControl.__init__(self, user)
+        self.resource_name = resource_name
+
+    def read(self):
+        if self.has_permission("read"):
+            self.log(f"{self.user} прочитал ресурс {self.resource_name}.")
+            print(f"Чтение ресурса {self.resource_name}")
+        else:
+            self.log(f"{self.user} попытался прочитал ресурс {self.resource_name}.")
+            print(f"ошибка доступа к ресурсу {self.resource_name}")
+
+    def write(self, data):
+        if self.has_permission("write"):
+            self.log(f"{self.user} записал в ресурс  {self.resource_name}.")
+            print(f"запись завершенга {data}")
+        else:
+            self.log(f"{self.user} попытался записать в ресурс  {self.resource_name}.")
+            print(f"ошибка доступа к ресурсу {self.resource_name}")
+
+
+resource = SecureResource(user="Alice", resource_name="pharmacyDataBase")
+resource.read()
+resource.write("Paracetomol")
+resource.change_permission("write", True)
+resource.write("Paracetomol")
+
+for log in resource.logs:
+    print(log)
